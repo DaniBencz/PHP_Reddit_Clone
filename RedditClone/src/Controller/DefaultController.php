b7f196d0e5d0a011b0d18819e94f0ee1d8d14d56
Daniel Bencz <daniel.bencz@gmail.com>
2019-06-19 15:52:13 +0200

    able to save to database

--- RedditClone/src/Controller/DefaultController.php
+++ RedditClone/src/Controller/DefaultController.php
@@ -5,22 +5,49 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Bundle\FrameworkBundle\Entity\Post;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\PostRepository;
+use App\Entity\Post;
 
 class DefaultController extends AbstractController
 {
+  private $postRepository;
+
+  function __construct(PostRepository $postRepository)
+  {
+    $this->postRepository = $postRepository;
+  }
+
   /**
-   * @Route("/", name="calorie_index", methods={"GET"})
+   * @Route("/", name="reddit_index", methods={"GET"})
    */
   function index()
   {
+    //$post = $this->postRepository->findAll();
     return $this->render('index.html.twig');
   }
 
   /**
-   * @Route("/submit", name="save_food", methods={"GET"})
+   * @Route("/submitpage", name="submitpage", methods={"GET"})
    */
   function submitPage()
   {
-    return $this->render('submit.html.twig');
+    return $this->render('submitpage.html.twig');
+  }
+
+  /**
+   * @Route("/save", name="save_post", methods={"POST"})
+   */
+  function save(Request $request)
+  {
+    $post = new Post();
+    $post->setTitle($request->request->get('title'));
+    $post->setUrl($request->request->get('url'));
+
+    $entityManager = $this->getDoctrine()->getManager();
+    $entityManager->persist($post);
+    $entityManager->flush();
+    return $this->redirectToRoute('reddit_index');
   }
 }
